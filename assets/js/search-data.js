304c6b29ea69b19e5d4f2b7e525da89771cd9aea
zghodsi <zghodsi@users.noreply.github.com>
2025-09-02 00:44:43 +0000

    Deploying to gh-pages from @ zghodsi/zghodsi.github.io@5894aad1b804ec0e59d81371f75266d4806b7582 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,7 +63,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%61%68%72%61@%70%75%72%64%75%65.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
